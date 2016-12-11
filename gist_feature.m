f9d16694a575bbb50354a85d69247ea7555f879e
oppsitre <oppsitre@163.com>
2016-12-11 17:27:16 +0800

    first commit

---
+++ gist_feature.m
@@ -0,0 +1,55 @@
+clear param
+param.orientationsPerScale = [8 8 8 8]; % number of orientations per scale (from HF to LF)
+param.numberBlocks = 1;
+param.fc_prefilt = 4;
+load_path = '/media/lcc/Windows/Downloads/SSRL_SKY/';
+file_list = [];
+feat_list = [];
+for y = 1999:2016
+    for m = 1:12
+        if m < 10
+            month_path = strcat(load_path, int2str(y), '/', '0', int2str(m),'/');
+        else
+            month_path = strcat(load_path, int2str(y), '/', int2str(m),'/');
+        end
+        files = dir(month_path);
+        imgDataPath = month_path;
+        imgDataDir  = dir(imgDataPath);             % 遍历所有文件
+        for i = 1:length(imgDataDir)
+            if(isequal(imgDataDir(i).name,'.')||... % 去除系统自带的两个隐文件夹
+                isequal(imgDataDir(i).name,'..')||...
+                ~imgDataDir(i).isdir)                % 去除遍历中不是文件夹的
+            continue;
+            end
+            imgDir = dir([imgDataPath imgDataDir(i).name '/*jpg']); 
+            for j =1:length(imgDir)                 % 遍历所有图片
+                str = [imgDataPath imgDataDir(i).name '/' imgDir(j).name]
+                img = imread(str);
+                [gist, param] = LMgist(img, '', param);
+                file_list = [file_list;imgDir(j).name(1:12)];
+                feat_list = [feat_list;gist];
+%                 size(file_list)
+%                 size(feat_list)
+            end
+        end
+    end
+end
+csv.write('exist_file_list.csv', file_list);
+csv.write('feat_list.csv', feat_list);
+% 
+% % 计算GIST
+% 
+% imgDataPath = load_path;
+% imgDataDir  = dir(imgDataPath);             % 遍历所有文件
+% for i = 1:length(imgDataDir)
+%     if(isequal(imgDataDir(i).name,'.')||... % 去除系统自带的两个隐文件夹
+%        isequal(imgDataDir(i).name,'..')||...
+%        ~imgDataDir(i).isdir)                % 去除遍历中不是文件夹的
+%            continue;
+%     end
+%     imgDir = dir([imgDataPath imgDataDir(i).name '/*jpg']); 
+%     for j =1:length(imgDir)                 % 遍历所有图片
+%         str = [imgDataPath imgDataDir(i).name '/' imgDir(j).name];
+%         disp(str);
+%     end
+% end
---
+++ preprocessing.py
@@ -0,0 +1,173 @@
+#-*- coding: utf-8 -*-
+'''
+According to the paper, the authors extracted upto 80 frames from each video,
+they did not mention if they grabbed first 80 frames, or sampled 80 frames with same intervals,
+but anyway I did the latter.
+'''
+import cv2
+import os
+import ipdb
+import numpy as np
+import pandas as pd
+from skimage import feature
+from cnn_util import *
+
+image_load_path = '/media/lcc/Windows/Downloads/SSRL_SKY/'
+image_save_path = '/media/lcc/Windows/Downloads/SSRL_SKY/feat/'
+year = range(1999, 2017)
+def preprocess_frame(image, target_height=224, target_width=224):
+
+    if len(image.shape) == 2:
+        image = np.tile(image[:,:,None], 3)
+    elif len(image.shape) == 4:
+        image = image[:,:,:,0]
+
+    image = skimage.img_as_float(image).astype(np.float32)
+    height, width, rgb = image.shape
+    if width == height:
+        resized_image = cv2.resize(image, (target_height,target_width))
+
+    elif height < width:
+        resized_image = cv2.resize(image, (int(width * float(target_height)/height), target_width))
+        cropping_length = int((resized_image.shape[1] - target_height) / 2)
+        resized_image = resized_image[:,cropping_length:resized_image.shape[1] - cropping_length]
+
+    else:
+        resized_image = cv2.resize(image, (target_height, int(height * float(target_width) / width)))
+        cropping_length = int((resized_image.shape[0] - target_width) / 2)
+        resized_image = resized_image[cropping_length:resized_image.shape[0] - cropping_length,:]
+
+    return cv2.resize(resized_image, (target_height, target_width))
+
+# def prerain_CNN():
+#     vgg_model = '/home/lcc/caffe/models/bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel'
+#     vgg_deploy = '/home/lcc/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt'
+#     year = range(1999,2015)
+#
+#     #videos = filter(lambda x: x.endswith('avi'), videos)
+#     width = 227
+#     height = 227
+#     print 'Before CNN'
+#     cnn = CNN(model=vgg_model, deploy=vgg_deploy, width=width, height=height)
+#     print 'After CNN'
+#     for y in year[::-1]:
+#         print image_load_path + str(y) + '/'
+#         feat_list = []
+#         for parent, dirnames, filenames in os.walk(image_load_path+str(y)+'/'):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+#             for filename in filenames:  # 输出文件信息
+#                 if not filename.endswith('.jpg'):
+#                     continue
+#                 # print "parent is:" + parent
+#                 # print "filename is:" + filename
+#                 # print "the full name of the file is:" + os.path.join(parent, filename)  # 输出文件路径信息
+#                 file = os.path.join(parent, filename)
+#                 img_list = []
+#                 img = cv2.imread(file)
+#                 if img is None:
+#                     continue
+#                 img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+#                 img_list.append(img)
+#                 img_list = np.array(img_list)
+#                 feat = cnn.get_features(img_list,layers='fc7',layer_sizes=[4096])[0].tolist()
+#                 print filename[:-4]
+#                 feat.insert(0, int(filename[:-4]))
+#                 feat_list.append(feat)
+#         feat_list = np.array(feat_list)
+#         print feat_list.shape
+#         np.savetxt(image_save_path+str(y)+"_rcnnet.csv", feat_list, delimiter=",")
+
+def Dense_sift(img):
+    img = cv2.resize(img,(25,25))
+    #dense = cv2.FeatureDetector_create('Dense')
+    dense = cv2.FeatureDetector_create('Dense')
+    f = '{} ({}): {}'
+    for param in dense.getParams():
+        type_ = dense.paramType(param)
+        if type_ == cv2.PARAM_BOOLEAN:
+            print f.format(param, 'boolean', dense.getBool(param))
+        elif type_ == cv2.PARAM_INT:
+            print f.format(param, 'int', dense.getInt(param))
+        elif type_ == cv2.PARAM_REAL:
+            print f.format(param, 'real', dense.getDouble(param))
+        else:
+            print param
+    #dense = cv2.setDouble('')
+    dense.setDouble('initFeatureScale', 10)
+    dense.setDouble('featureScaleMul', 10)
+    dense.setInt('initXyStep', 10)
+    print type(dense)
+    kp = dense.detect(img)
+    print len(kp)
+    sift = cv2.SIFT()
+    kp,des = sift.compute(img,kp)
+    print des.ravel().shape
+    return des
+    #print type(kp[0]), type(des[0])
+    #print len(kp), len(des)
+
+def HOG_feature(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(gray,(64,128))
+    hog = cv2.HOGDescriptor()
+    h = hog.compute(img)
+    h = np.reshape(h, (1, h.shape[0]))
+    return h.tolist()
+
+
+class LocalBinaryPatterns:
+    def __init__(self, numPoints, radius):
+        # store the number of points and radius
+        self.numPoints = numPoints
+        self.radius = radius
+
+    def describe(self, image, eps=1e-7):
+        # compute the Local Binary Pattern representation
+        # of the image, and then use the LBP representation
+        # to build the histogram of patterns
+        lbp = feature.local_binary_pattern(image, self.numPoints,
+                                           self.radius, method="uniform")
+        (hist, _) = np.histogram(lbp.ravel(),
+                                 bins=np.arange(0, self.numPoints + 3),
+                                 range=(0, self.numPoints + 2))
+
+        # normalize the histogram
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+
+        # return the histogram of Local Binary Patterns
+        return hist
+def LBP_feature(img, numPoints = 24, radius = 8):
+    desc = LocalBinaryPatterns(numPoints, radius)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hist = desc.describe(gray)
+    #print hist.shape
+    return hist.tolist()
+def get_feature(method):
+    for y in year[::-1]:
+        print image_load_path + str(y) + '/'
+        feat_list = []
+        for parent, dirnames, filenames in os.walk(image_load_path + str(y) + '/'):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+            for filename in filenames:  # 输出文件信息
+                if not filename.endswith('.jpg'):
+                    continue
+                file = os.path.join(parent, filename)
+                img = cv2.imread(file)
+                if img is None:
+                    continue
+                print filename[:-4]
+                if method == 'HOG':
+                    feat = HOG_feature(img)
+                elif method == 'LBP':
+                    feat = LBP_feature(img)
+                elif method == 'DSIFT':
+                    feat = Dense_sift(img)
+                #feat.insert(0, int(filename[:-4]))
+                feat_list.append(feat)
+        feat_list = np.array(feat_list)
+        print feat_list.shape
+        np.savetxt(image_save_path + method + '/' + str(y) + '_' + method + '.csv', feat_list, delimiter=",")
+if __name__=="__main__":
+    #get_feature('LBP')
+    img = cv2.imread(image_load_path + '2016/01/01/201601011000.jpg')
+    fea = Dense_sift(img)
+    print len(fea)
