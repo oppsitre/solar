e3df9b6061d50415570e306f5d42a9c373d4b829
oppsitre <oppsitre@163.com>
2017-01-13 22:22:09 +0800

    t

---
+++ src/model_lstm_16.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+__author__= 'WANG Kejie<wang_kejie@foxmail.com>'
+__date__ = '12/10/2016'
+
+import tensorflow as tf
+#from cnn import cnn_model
+class Model:
+    """
+    This is an multi-modal (three modality) neutral network model used as point/probabilistic forecast
+        model digram:
+         ---------
+        |  lstm   |--------- |
+         ---------           |
+                             |
+         ---------           |           ----------           ------------
+        |  lstm   |--------- |----------|   lstm   | --------| regression |
+         ---------           |           ----------           ------------
+                             |
+         ---------           |
+        | CNN-lstm |---------|
+         ---------
+
+        The model focuses on the multi-modality and this model contain three modalities each of which is lstm, lstm and CNN.
+        e.g. This model used to predict solar irradiance and the first and second modalities are the irradiance and meteorological data
+            and the third modality is an image dataset and use an CNN to extract the feature. And then concatenating all features into a
+            feature as the input of the lstm of the second level. Then we use the output of the last cell as the regressor input to predict
+            the value.
+        regressor: there are lots of regressors can be used for different purpose. (specific in the config in )
+            e.g. linear regression, a fully connected NN with linear regression, support vector regression,
+                multi-support vector regression (considering the time dependency)
+                quantile regression (used as probabilistic regression)
+    """
+    def __init__(self, data, target, keep_prob, config):
+        """
+        @brief The constructor of the model
+        @param data: the input the data of the model (features) data[0], data[1], ... for multi-modality
+               target: the groundtruth of the model
+               keep_prob: use dropout to avoid overfitting (https://www.cs.toronto.edu/%7Ehinton/absps/JMLRdropout.pdf)
+               config: the configuration of the model and it may contains following values:
+                    n_first_hidden, n_second_hidden, n_hidden_level2, n_fully_connect_hidden, n_target: the params of the network
+                    lr: learning rate
+                    epsilon, C: params for epsilon-insensitive multi-support regression
+        """
+        #load the config
+        #the input data
+        self.data = data
+        #self.files = files
+        self.target = target
+        self.keep_prob = keep_prob
+        self.image_data = data[0]
+        self.cnn_feat_size = config.cnn_feat_size
+
+        #the network parameters
+        self.n_third_hidden = config.n_third_hidden
+        self.n_hidden_level2 = config.n_hidden_level2
+        self.n_fully_connect_hidden = config.n_fully_connect_hidden
+        self.n_target = config.n_target
+        self.n_step = config.n_step
+
+        #train params
+        self.lr = config.lr
+
+        #loss params (svr params)
+        self.epsilon = config.epsilon
+        self.C = config.C
+
+        self._prediction = None
+        self._optimize = None
+        self._loss = None
+
+        self.input_width = config.width
+        self.input_height = config.heigth
+        self.output_size = config.cnn_feat_size
+
+    def weight_varible(self, shape):
+        # initial = tf.truncated_normal(shape, stddev=0.1)
+        weights = tf.get_variable('weights', shape, initializer=tf.random_normal_initializer(0, stddev=0.1))
+        return weights
+
+    def bias_variable(self, shape):
+        # initial = tf.constant(0.1, shape=shape)
+        bias = tf.get_variable('bias', shape, initializer=tf.constant_initializer(0.1))
+        return bias
+
+    def conv2d(self, x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    def cnn_model(self, x, keep_prob):
+        x_image = tf.reshape(x, [-1, self.input_height, self.input_width, 1])
+        with tf.variable_scope('conv_1') as scope:
+            W_conv1 = self.weight_varible([5, 5, 1, 32])
+            b_conv1 = self.bias_variable([32])
+
+            # conv layer-1
+            # x = tf.placeholder(tf.float32, [None, 784])
+            h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
+            h_pool1 = self.max_pool_2x2(h_conv1)
+
+            axis = list(range(len(h_pool1.get_shape()) - 1))
+            mean, variance = tf.nn.moments(h_pool1, axis)
+            #print mean.get_shape(), variance.get_shape()
+            batch_normal_1 = tf.nn.batch_normalization(h_pool1, mean, variance, None, None, 0.001)
+            #return h_pool1
+            # print W_conv1.name, W_conv1.get_shape()
+            # print b_conv1.name, b_conv1.get_shape()
+            # print h_conv1.name, h_conv1.get_shape()
+            #print h_pool1.name, h_pool1.get_shape()
+            #
+            # print W_conv1.name
+            # print b_conv1.name
+            # print h_conv1.name
+            # print h_pool1.name
+
+        with tf.variable_scope('conv_2') as scope:
+            # conv layer-2
+            W_conv2 = self.weight_varible([5, 5, 32, 64])
+            b_conv2 = self.bias_variable([64])
+
+            h_conv2 = tf.nn.relu(self.conv2d(batch_normal_1, W_conv2) + b_conv2)
+            h_pool2 = self.max_pool_2x2(h_conv2)
+
+            axis = list(range(len(h_pool2.get_shape()) - 1))
+            mean, variance = tf.nn.moments(h_pool2, axis)
+            #print mean.get_shape(), variance.get_shape()
+
+            batch_normal_2 = tf.nn.batch_normalization(h_pool2, mean, variance, None, None, 0.001)
+
+        # with tf.variable_scope('conv_3') as scope:
+        #     # conv layer-2
+        #     W_conv3 = self.weight_varible([5, 5, 64, 64])
+        #     b_conv3 = self.bias_variable([64])
+        #
+        #     h_conv3 = tf.nn.relu(self.conv2d(batch_normal_2, W_conv3) + b_conv3)
+        #     h_pool3 = self.max_pool_2x2(h_conv3)
+        #
+        #     axis = list(range(len(h_pool3.get_shape()) - 1))
+        #     mean, variance = tf.nn.moments(h_pool3, axis)
+        #     #print mean.get_shape(), variance.get_shape()
+        #
+        #     batch_normal_3 = tf.nn.batch_normalization(h_pool3, mean, variance, None, None, 0.001)
+
+        with tf.variable_scope('fc_1') as scope:
+            # full connection
+            s = batch_normal_2.get_shape().as_list()
+            #print 'X1,X2,X3,X4', s
+            W_fc1 = self.weight_varible([s[1]*s[2]*s[3], self.output_size])
+            b_fc1 = self.bias_variable([self.output_size])
+
+            batch_normal_flat = tf.reshape(batch_normal_2, [-1, s[1]*s[2]*s[3]])
+            h_fc1 = tf.nn.relu(tf.matmul(batch_normal_flat, W_fc1) + b_fc1)
+
+            # dropout
+            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+            # print W_fc1.name, W_fc1.get_shape()
+            # print b_fc1.name, b_fc1.get_shape()
+            # print h_pool2_flat.name, h_pool2_flat.get_shape()
+            # print h_fc1_drop.name, h_fc1_drop.get_shape()
+
+            return h_fc1_drop
+    @property
+    def prediction(self):
+        """
+        Build the graph of the model and return the prediction value of the model
+        NOTE: You can easily treat it as an member of this class (model.prediction to refer)
+        """
+        print '..................', self.data[0].get_shape()
+
+        if self._prediction is None:
+            # build the graph
+            # cnn_out = None
+            # with tf.variable_scope('image') as scope:
+            #     for i in range(self.n_step):
+            #         tmp_out = self.cnn_model(self.image_data[:, i, :, :], self.keep_prob)
+            #         tmp_out = tf.reshape(tmp_out, [-1, 1, self.cnn_feat_size])
+            #         if cnn_out is None:
+            #             cnn_out = tmp_out
+            #         else:
+            #             cnn_out = tf.concat(1, [tmp_out,cnn_out])
+            #         scope.reuse_variables()
+            #         #print '!!!!!', cnn_out.name, cnn_out.get_shape()
+            #
+            #         # h_fc1_drop_2 = cnn_model(x, keep_prob)
+            #         # h_fc1_drop_2 = h_fc1_drop_2[:,:512]
+            #         # print h_fc1_drop_2.name
+            # print cnn_out.name, cnn_out.get_shape()
+
+            with tf.variable_scope("first_level3"):
+                cell_3 = tf.nn.rnn_cell.LSTMCell(self.n_third_hidden, state_is_tuple=True)
+                outputs_3, state3 = tf.nn.dynamic_rnn(cell_3, self.image_data, dtype=tf.float32)
+
+            #2nd level lstm
+            with tf.variable_scope("second_level"):
+                cell_level2 = tf.nn.rnn_cell.LSTMCell(self.n_hidden_level2, state_is_tuple=True)
+                outputs, state_level2 = tf.nn.dynamic_rnn(cell_level2, outputs_3, dtype=tf.float32)
+
+
+            #outputs: [batch_size, n_step, n_hidden] -->> [n_step, batch_size, n_hidden]
+            #output: [batch_size, n_hidden]
+            #get the last output of the lstm as the input of the regressor
+            outputs = tf.transpose(outputs, [1, 0, 2])
+            output = tf.gather(outputs, int(outputs.get_shape()[0]) - 1)
+
+            # regression
+            w_fc1 = tf.Variable(tf.truncated_normal([self.n_hidden_level2, self.n_fully_connect_hidden]), dtype=tf.float32)
+            b_fc1 = tf.Variable(tf.constant(0.1, shape=[self.n_fully_connect_hidden]), dtype=tf.float32)
+            h_fc1 = tf.sigmoid(tf.matmul(output, w_fc1) + b_fc1)
+
+            # h_fc_drop = tf.nn.dropout(h_fc1, self.keep_prob)
+
+            #multi-support vector regresiion
+            self.weight = tf.Variable(tf.truncated_normal([self.n_fully_connect_hidden, self.n_target]), dtype=tf.float32)
+            bias = tf.Variable(tf.constant(0.1, shape=[self.n_target]), dtype=tf.float32)
+
+            self._prediction = tf.matmul(h_fc1, self.weight) + bias
+
+        return self._prediction
+
+    @property
+    def loss(self):
+        """
+        Define the loss of Fthe model and you can modify this section by using different regressor
+        """
+        if self._loss is None:
+            #compute the ||w||2
+            #use the w^T * W to compute and the sum the diag to get the result
+            m = tf.matmul(tf.transpose(self.weight,[1,0]), self.weight)
+            diag = tf.matrix_diag_part(m)
+            w_sqrt_sum = tf.reduce_sum(diag)
+
+            #the loss of the trian set
+            diff = self.prediction - self.target
+            err = tf.sqrt(tf.reduce_sum(tf.square(diff), reduction_indices=1)) - self.epsilon
+            err_greater_than_espilon = tf.cast(err > 0, tf.float32)
+            total_err = tf.reduce_sum(tf.mul(tf.square(err), err_greater_than_espilon))
+
+            self._loss = 0.5 * w_sqrt_sum + self.C * total_err
+            # self._loss = total_err
+        return self._loss
+
+
+    @property
+    def optimize(self):
+        """
+        Define the optimizer of the model used to train the model
+        """
+        if self._optimize is None:
+            optimizer = tf.train.MomentumOptimizer(self.lr,0.9)
+            self._optimize = optimizer.minimize(self.loss)
+        return self._optimize
---
+++ src/solar_prediction_feature_lstm.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+__author__= 'WANG Kejie<wang_kejie@foxmail.com>'
+__date__ = '21/11/2016'
+
+import tensorflow as tf
+import numpy as np
+import json
+from collections import namedtuple
+from reader_feature_lstm import Reader
+from model_lstm_16 import Model
+from util import MSE_And_MAE, test_figure_plot
+import sys
+import datetime
+import time
+t = time.strftime('%m%d%H%M',time.localtime(time.time()))
+#log_file = open('log_'+ t,'w')
+#sys.stdout = log_file
+def main(_):
+    #get the config
+    print time.localtime()
+    fp = open('../config.json')
+    config = json.load(fp, object_hook=lambda d:namedtuple('X', d.keys())(*d.values()))
+    fp.close()
+
+    n_step = config.n_step
+    n_target = config.n_target
+    width_image = config.width
+    height_image = config.heigth
+    epoch_size = config.epoch_size
+    print_step = config.print_step
+
+    test_num = config.test_num
+
+    #define the input and output
+    x_sky_cam = tf.placeholder(tf.float32, [None, n_step, 16])
+    #x_sky_cam_path = tf.placeholder(tf.str, [None, n_step, 1])
+    y_ = tf.placeholder(tf.float32, [None, n_target])
+    keep_prob = tf.placeholder(tf.float32)
+
+    reader = Reader(config)
+
+    model = Model([x_sky_cam], y_, keep_prob, config)
+    prediction = model.prediction
+    loss = model.loss
+    optimize = model.optimize
+
+    init_op = tf.global_variables_initializer()
+    #new a saver to save the model
+    saver = tf.train.Saver()
+
+    validation_last_loss = float('inf')
+
+    with tf.Session() as sess:
+        # initialize all variables
+        #tf.initialize_all_variables().run()
+        #sess.run(init_op)
+        save_path = './feature_lstm_model.ckpt'
+        saver.restore(sess, save_path)
+        for i in range(epoch_size):
+            print 'Epoch:', i
+
+            if i%config.test_step == 0:
+                sky_cam_test_input, test_target = reader.get_test_set(test_num)
+                test_feed = {x_sky_cam: sky_cam_test_input, keep_prob:1.0}
+                test_result = sess.run(prediction, feed_dict=test_feed)
+                #'connected'
+                #calculate the mse and mae
+                mse, mae = MSE_And_MAE(test_target, test_result)
+                print "Test MSE: ", mse
+                print "Test MAE: ", mae
+
+                sky_cam_train_input, train_target = reader.next_batch()
+                train_feed = {x_sky_cam: sky_cam_train_input, keep_prob:1.0}
+                train_result = sess.run(prediction, feed_dict=train_feed)
+                mse, mae = MSE_And_MAE(train_target, train_result)
+                print "Train MSE: ", mse
+                print "Train MAE: ", mae
+
+            batch = reader.next_batch()
+            train_feed = {x_sky_cam: batch[0], y_:batch[1], keep_prob:0.5}
+            sess.run(optimize, feed_dict=train_feed)
+
+
+            if i%config.print_step == 0:
+                print "train loss:",sess.run(loss, feed_dict=train_feed)
+                print "validation loss: ", validation_last_loss
+
+            #validation
+            validation_set = reader.get_validation_set()
+            validation_feed = {x_sky_cam: validation_set[0], y_:validation_set[1], keep_prob:0.5}
+            validation_loss = sess.run(loss,feed_dict=validation_feed)
+
+            #compare the validation with the last loss
+            if(validation_loss < validation_last_loss):
+                validation_last_loss = validation_loss
+
+            sp = saver.save(sess, save_path)
+            print("Model saved in file: %s" % sp)
+
+
+if __name__ == "__main__":
+    tf.app.run()
